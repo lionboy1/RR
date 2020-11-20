d68803ad415c223297d45ad332d96299383c793b
lionboy1 <great.studios30@gmail.com>
2020-11-20 00:28:33 -0500

    refactored scripts. player now faces at enemies during attack

--- Scripts/Combat/Fighter.cs
+++ Scripts/Combat/Fighter.cs
@@ -10,8 +10,14 @@ namespace RR.Combat
 
         [SerializeField]
         float weaponRange = 2.0f;
-        Transform target;
+        [SerializeField]
+        float timeBetweenAttacks = 1.0f;
+        float timeSinceLastAttack = 0;
+        Health target;
         Mover _mover;
+        Animator _anim;
+        [SerializeField]
+        float _weaponDamage;
         
         ActionScheduler _actionScheduler;
         
@@ -28,37 +34,56 @@ namespace RR.Combat
             {
                 Debug.LogError("ActionScheduler component not found");
             }
+            _anim = GetComponent<Animator>();
+            if( _anim == null)
+            {
+                Debug.LogError("Animator component not attached!");
+            }
         }
         void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if(target == null) return;//resume to normal movement
-            
-            //_mover.MoveTo(target.position);
+            if(target.IsDead()) return;
             //If in range of target
             if (target != null && !RangeCheck())
             {
-                _mover.MoveTo(target.position);
+                _mover.MoveTo(target.transform.position);
             }
             //If not close enough to target yet...
             else
             {
                 _mover.Cancel();
+                //Add animator code here for attack animation
+                AttackBehavior();
             }
         }
 
         private bool RangeCheck()
         {
-            return Vector3.Distance(target.position, transform.position) < weaponRange;
+            return Vector3.Distance(target.transform.position, transform.position) < weaponRange;
         }
 
         public void Attack(CombatTarget combatTarget)
         {
             _actionScheduler.StartAction(this);
-            target = combatTarget.transform;
+            target = combatTarget.GetComponent<Health>();
+            target.GetComponent<Health>().Damage(3f);
         }
         public void Cancel()
         {
             target = null;
+        }
+        void AttackBehavior()
+        {
+            transform.LookAt(target.transform);
+            //Throttle attacks
+            if(timeSinceLastAttack > timeBetweenAttacks)    
+            {   
+                _anim.SetTrigger("attack");
+                timeSinceLastAttack = 0;
+                target.Damage(_weaponDamage);
+            }
         }
     }
 }
---
+++ Scripts/Combat/Health.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RR.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField]
+        float _health = 100f;
+        Animator _anim;
+        bool isDead = false;
+
+        //Getter so other classes can query is target is dead or not
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
+        void Start()
+        {
+            _anim = this.GetComponent<Animator>();
+            if(_anim == null)
+            {
+                Debug.LogError("Animator not found");
+            }
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void Damage(float damage)
+        {
+            _health = Mathf.Max(_health -= damage, 0);//Reduces health but not under 0
+            if(_health == 0)
+            {
+               Die();
+            }
+            
+        }
+        void Die()
+        {
+            if (isDead) return;//back out of the function is isDead is already true;
+                
+            //Otherwise, continue
+            _anim.SetTrigger("die");
+            isDead = true;
+        }
+    }
+}
+
