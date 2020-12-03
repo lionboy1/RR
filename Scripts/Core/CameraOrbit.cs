52d7a057ab7db9ed8adb6745b127cab7fde496c2
lionboy1 <great.studios30@gmail.com>
2020-12-03 00:35:59 -0500

    Camera rotation

---
+++ Scripts/Core/CameraOrbit.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace RR.Core
+{
+    public class CameraOrbit : MonoBehaviour 
+    {
+        
+        [SerializeField] float rotSpeed;
+        [SerializeField] Transform target;
+        [SerializeField] float zoomIn;
+        [SerializeField] float zoomOut;
+        [SerializeField] float zoomLerpSpeed = 2.0f;
+        Camera cam;
+        bool isZoomed = false;
+        
+        void Start()
+        {
+            cam = Camera.main;
+            //targetZoom = cam.orthographicSize;
+        }
+        void Update()
+        {
+            RotateCamera();
+            RightClickZoom();
+        }
+
+        void RotateCamera()
+        {
+            if(Input.GetKey(KeyCode.Q))
+            {
+                transform.RotateAround(target.transform.position, -Vector3.up, rotSpeed * Time.deltaTime);
+            }
+            if(Input.GetKey(KeyCode.E))
+            {
+                transform.RotateAround(target.transform.position, Vector3.up, rotSpeed * Time.deltaTime);
+            }
+        }
+        void RightClickZoom()
+        {
+            //float scroll = Input.GetAxis("Mouse ScrollWheel");//For taking in horizontal input
+
+            if(Input.GetMouseButtonDown(1))
+            {
+                isZoomed = !isZoomed;
+            }
+            if(isZoomed)
+            {
+                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, zoomIn, zoomLerpSpeed * Time.deltaTime);
+            }
+            else
+            {
+                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, zoomOut, zoomLerpSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
+
+
+
+
+
+
--- Scripts/Core/FollowCamera.cs
+++ Scripts/Core/FollowCamera.cs
@@ -4,14 +4,17 @@ namespace RR.Core
 {
     public class FollowCamera : MonoBehaviour
     {
-        [SerializeField]
-        Transform target;
+        [SerializeField] Transform target;
+        
     
         // Update is called once per frame
         void LateUpdate()
         {
             this.transform.position = target.position;
+            //float horizontalInput = Input.GetAxis("Horizontal");
+            
         }
+        
     }
 }
 
