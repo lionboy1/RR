943e6e3859ebc1b0a7ef8a8586fe83419de99e0e
lionboy1 <great.studios30@gmail.com>
2021-07-18 04:21:19 -0400

    Created health slider script for player

---
+++ Scripts/UI/HealthSlider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RR.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Only the player health should reference this, not the AI's.
+
+///Borrowed the idea from https://www.youtube.com/watch?v=BLfNP4Sc_iA
+
+public class HealthSlider : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fillableImage;
+
+    void Start()
+    {
+        SetMaxHealth(100);
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fillableImage.color = gradient.Evaluate(1f);
+    }
+
+    // Update is called once per frame
+    public void UpdateHealth(int healthAmount)
+    {
+        slider.value = healthAmount;
+        fillableImage.color = gradient.Evaluate(slider.normalizedValue);//translates to 0-1.
+    }
+}
