6f871f6139d723360c87d27e8509f54beb5b857c
lionboy1 <great.studios30@gmail.com>
2021-11-15 00:36:54 -0500

    fixed aggro cool down time

--- Scripts/Control/AIController.cs
+++ Scripts/Control/AIController.cs
@@ -11,16 +11,17 @@ namespace RR.Control
     [RequireComponent(typeof(ActionScheduler))]
     public class AIController : MonoBehaviour 
     {
-        [SerializeField] float chaseDistance = 5f;
-        [SerializeField] float _suspicionTime = 10f;
-        [SerializeField] float _aggroCooldownTime = 10f;
+        [SerializeField] float chaseDistance;
+        [SerializeField] float _suspicionTime;
+        //Make the aggro time higher than suspicion time
+        [SerializeField] float _aggroCooldownTime;
         [SerializeField] PatrolPath _patrolPath;
         [SerializeField] float waypointTolerance = 1.0f;
         [SerializeField] float waypointDwellTime = 3.0f;
 
         [Range(0,1)]//This limits the range of the patrolSpeedFraction value below.
         [SerializeField] float patrolSpeedFraction = 0.2f;//Percent of max speed in Mover.cs to apply for waling.
-        [SerializeField] float shoutDistance = 5f;
+        [SerializeField] float shoutDistance;
         Mover _mover;
         Fighter _fighter;
         GameObject player;
@@ -72,15 +73,17 @@ namespace RR.Control
             if (IsAggravated() && _fighter.CanAttack(player))
             {
                 AttackBehavior();
+                Debug.Log("attacking");
             }
             else if (_timeSinceLastSawPlayer < _suspicionTime)
             {
                 //Suspiscion state
+                Debug.Log("suspiscious");
                 SuspicionBehavior();
             }
             else
             {
-                PatrolBehavior();//will automaticall cancel currenc action.
+                PatrolBehavior();//will automaticall cancel current action.
             }
             UpdateTimers();
         }
@@ -90,8 +93,16 @@ namespace RR.Control
             timeSinceAggravated = 0;
         }
 
+        bool IsAggravated()
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            //Check if aggravation timer has expired
+            return distanceToPlayer < chaseDistance || timeSinceAggravated <_aggroCooldownTime;
+        }
+
         private void UpdateTimers()
         {
+            timeSinceAggravated += Time.time;//delta time increment too sall and wasn't working
             _timeSinceLastSawPlayer += Time.deltaTime;//Count time since last saw.
             _timeSinceArrivedAtWaypoint += Time.deltaTime;
         }
@@ -158,12 +169,7 @@ namespace RR.Control
             }
         }
 
-        bool IsAggravated()
-        {
-            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-            //Check if aggravation timer has expired
-            return distanceToPlayer < chaseDistance || timeSinceAggravated <_aggroCooldownTime;
-        }
+        
         //Called by Unity
         //Creates a gizmo to see enemy radius
         void OnDrawGizmosSelected()
